00b91b03078048b1aa1bc29105b6ba3522803bb2
lay2lo <danielomotayo773@gmail.com>
2024-12-04 07:38:23 +0100

    Daniel added collection of data structure

---
+++ datastructure.py
@@ -0,0 +1,47 @@
+# # number=[13,21,6,7,90]
+# okpa_list=['bambara nut'
+#           ,'maggie',
+#            'palm oil',
+#            'vegetable',
+#            ]
+# print(okpa_list)
+# okpa_list.append('fish')
+#
+# print(okpa_list)
+#
+# ingredient = input('enter an ingredients ')
+#
+# for item in okpa_list:
+#     if item== ingredient:
+#         print(item,'is in the list')
+#     else:
+# #
+#
+number_list = [13,17,25,60,98,16,65,32,78,25]
+for i in (len(number_list) -1, -2, -3, -4, -5, -6, -7, -8, -9, -10):
+    print(number_list[i])
+
+# yoruba_dictionary = {
+#     'iya': 'mother',
+#     'iresi': 'rice',
+#     'owo': 'money',
+#     'ori':'head',
+#     'aja':'dog',
+#     'oju':'eye',
+#     'dudu':'black',
+#     'ese':'thanks',
+#     'wa':'come',
+#     'lo':'go'
+# }
+#
+# word= input('enter a yoruba word')
+# print(yoruba_dictionary[word])
+
+number_list = [13,17,25,60,98,16,65,32,78,25]
+for i in (len(number_list) -1, -2, -3, -4, -5, -6, -7, -8, -9, -10):
+    print(number_list[i])
+
+
+
+
+
